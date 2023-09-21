3b2191560a719076cc8357498b3973b53a1fa97f
farrasmf <farrasmuhammadfurqon@gmail.com>
2023-09-21 20:14:01 +0700

    new

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const router = require("./routers/router");
+const port = 3000;
+
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Server berjalan di port ${port}`); 
+});
+
